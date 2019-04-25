f2d271821372176a79f729b52cef1b73383bec5e
Peter Milanovits/BDP/HU <peter.milanovits@avon.com>
2019-04-25 09:03:57 +0200

    Updated for live env

--- background.js
+++ background.js
@@ -1,11 +1,17 @@
 chrome.runtime.onInstalled.addListener(function () {
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: { hostEquals: "qafstgadm-mab.avon.com" },
-      })
+      conditions: [
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: "qafstgadm-mab.avon.com" }
+        }),
+        new chrome.declarativeContent.PageStateMatcher({
+          pageUrl: { hostEquals: "stgadm-eu.avon.com" }
+        })
       ],
-      actions: [new chrome.declarativeContent.ShowPageAction()]
+      actions: [
+        new chrome.declarativeContent.ShowPageAction()
+      ]
     }]);
   });
 });
--- popup.js
+++ popup.js
@@ -1,31 +1,63 @@
 let getUniqueId = document.querySelector("#getUniqueId")
+let successText = document.querySelector("#successText")
+let successTextID = document.querySelector("#successTextID")
+let warningText = document.querySelector("#warningText")
 
 const copyToClipboard = str => {
-  const el = document.createElement('textarea');
-  el.value = str;
-  el.setAttribute('readonly', '');
-  el.style.position = 'absolute';
-  el.style.left = '-9999px';
-  document.body.appendChild(el);
-  el.select();
-  document.execCommand('copy');
-  document.body.removeChild(el);
-};
+  const el = document.createElement("textarea")
+  el.value = str
+  el.setAttribute("readonly", "")
+  el.style.position = "absolute"
+  el.style.left = "-9999px"
+  document.body.appendChild(el)
+  el.select()
+  document.execCommand("copy")
+  document.body.removeChild(el)
+}
 
-getUniqueId.onclick = function (element) {
-  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+const showSuccessMessage = text => {
+  successTextID.innerHTML = text
+  successText.style.display = "block"
+  setTimeout(hideSuccessMessage, 3000)
+}
+
+const hideSuccessMessage = () => {
+  successText.style.display = "none"
+}
+
+const showWarningMessage = () => {
+  warningText.style.display = "block"
+  setTimeout(hideWarningMessage, 5000)
+}
+
+const hideWarningMessage = () => {
+  warningText.style.display = "none"
+}
+
+getUniqueId.onclick = element => {
+  chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
     chrome.tabs.executeScript(
       tabs[0].id,
       {
-        code: 'var string1 = document.querySelector(\'[modulename="cmc/foundation/TextViewer"]\').innerHTML; ' +
-          'chrome.runtime.sendMessage({ text: string1 });'
+        code: 'var elem = "";' +
+          'if (typeof elem !== \'undefined\' || elem !== null) { elem = document.querySelector(\'[modulename="cmc/foundation/TextViewer"]\') } else { var elem = document.querySelector(\'[modulename="cmc/foundation/TextViewer"]\') };' +
+          'if (elem != null) { chrome.runtime.sendMessage({ text: elem.innerHTML }) } else { chrome.runtime.sendMessage({ text: "noidfound" }) };'
       }
-    );
-  });
-};
+    )
+  })
+}
 
 chrome.runtime.onMessage.addListener(function (msg) {
-  if (msg.text !== undefined) {
+  if (msg.text !== undefined && msg.text !== "noidfound") {
     copyToClipboard(msg.text)
+    showSuccessMessage(msg.text)
+  } else if (msg.text === "noidfound") {
+    showWarningMessage()
   }
-});
+})
+
+// Login text for testing:
+// document.querySelector(\'[modulename="cmc/shell/LogonInputWidget::_$1_$0_$0"]\')
+//
+// Normal query:
+// document.querySelector(\'[modulename="cmc/foundation/TextViewer"]\')
